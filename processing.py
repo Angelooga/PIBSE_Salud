9b9569c4d987787b6cbc1b56037712b871c992e4
Angelooga <141773267+Angelooga@users.noreply.github.com>
2024-08-19 15:20:07 -0600

    Update processing.py

--- processing.py
+++ processing.py
@@ -139,6 +139,7 @@ def count_values(df, group_by: str, col: str):
     new_df = df.groupby(by=group_by)[col].value_counts().reset_index()
     new_df["Porcentaje"] = round(100 * new_df['count']/new_df.groupby('Fecha')["count"]\
                                                     .transform('sum'), 1)
+    new_df["Porcentaje"] = new_df["Porcentaje"].astype(float)
 
     return new_df.rename(columns={"count": "Participantes"})
 
